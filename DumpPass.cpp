a49a53616b0f0489a9df74eb2daf615d914e9cd7
johannes <johannesmiesenhardt@gmail.com>
2026-02-11 08:35:40 +0100

    Clean up DumpPass

--- DumpPass.cpp
+++ DumpPass.cpp
@@ -1,10 +1,11 @@
-#include <llvm/Passes/PassBuilder.h>
-#include <llvm/Passes/PassPlugin.h>
 #include <llvm/IR/Analysis.h>
 #include <llvm/IR/PassManager.h>
 #include <llvm/Pass.h>
 #include <llvm/Passes/OptimizationLevel.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
 #include <llvm/Support/Debug.h>
+#include <llvm/Support/Compiler.h>
 #include <llvm/Support/raw_ostream.h>
 
 #include <functional>
@@ -13,37 +14,31 @@ using namespace llvm;
 
 namespace {
 
-// void visitor(Function &F) {
-//   errs() << "(llvm-tutor) Hello from: " << F.getName() << "\n";
-//   errs() << "(llvm-tutor)   number of arguments: " << F.arg_size() << "\n";
-// }
+	struct HelloWorld : PassInfoMixin<HelloWorld> {
+		PreservedAnalyses run(Module& M, ModuleAnalysisManager&)
+		{
+			M.print(errs(), nullptr, false, false);
+			return PreservedAnalyses::all();
+		}
 
-struct HelloWorld : PassInfoMixin<HelloWorld> {
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
-    M.print(errs(), nullptr, false, false);
-    // for (Function &f : M.functions()) {
-    //   visitor(f);
-    // }
-    return PreservedAnalyses::all();
-  }
+		static bool isRequired() { return true; }
+	};
 
-  static bool isRequired() { return true; }
-};
+}
 
-} // namespace
-
-llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
-          [](PassBuilder &PB) {
-            PB.registerFullLinkTimeOptimizationEarlyEPCallback(
-                [](ModulePassManager &Mgr, OptimizationLevel Level) {
-                  Mgr.addPass(HelloWorld());
-                });
-          }};
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo()
+{
+	return { LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
+		[](PassBuilder& PB) {
+			PB.registerFullLinkTimeOptimizationEarlyEPCallback(
+				[](ModulePassManager& Mgr, OptimizationLevel) {
+					Mgr.addPass(HelloWorld());
+				});
+		} };
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return getHelloWorldPluginInfo();
+llvmGetPassPluginInfo()
+{
+	return getHelloWorldPluginInfo();
 }
-
