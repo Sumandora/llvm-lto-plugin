d0725e063db30f2cc6e021a71640a7bf813b42bf
johannes <johannesmiesenhardt@gmail.com>
2026-02-23 10:04:24 +0100

    Rename Plugin internally from HelloWorld to DumpPass to prevent confusion

--- DumpPass.cpp
+++ DumpPass.cpp
@@ -4,8 +4,8 @@
 #include <llvm/Passes/OptimizationLevel.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Passes/PassPlugin.h>
-#include <llvm/Support/Debug.h>
 #include <llvm/Support/Compiler.h>
+#include <llvm/Support/Debug.h>
 #include <llvm/Support/raw_ostream.h>
 
 #include <functional>
@@ -14,7 +14,7 @@ using namespace llvm;
 
 namespace {
 
-	struct HelloWorld : PassInfoMixin<HelloWorld> {
+	struct DumpPass : PassInfoMixin<DumpPass> {
 		PreservedAnalyses run(Module& M, ModuleAnalysisManager&)
 		{
 			M.print(errs(), nullptr, false, false);
@@ -26,19 +26,23 @@ namespace {
 
 }
 
-llvm::PassPluginLibraryInfo getHelloWorldPluginInfo()
+llvm::PassPluginLibraryInfo getDumpPassPluginInfo()
 {
-	return { LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
-		[](PassBuilder& PB) {
+	return {
+		.APIVersion = LLVM_PLUGIN_API_VERSION,
+		.PluginName = "Dump Pass",
+		.PluginVersion = LLVM_VERSION_STRING,
+		.RegisterPassBuilderCallbacks = [](PassBuilder& PB) {
 			PB.registerFullLinkTimeOptimizationEarlyEPCallback(
 				[](ModulePassManager& Mgr, OptimizationLevel) {
-					Mgr.addPass(HelloWorld());
+					Mgr.addPass(DumpPass());
 				});
-		} };
+		},
+	};
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo()
 {
-	return getHelloWorldPluginInfo();
+	return getDumpPassPluginInfo();
 }
